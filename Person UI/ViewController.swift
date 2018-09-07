9ea113384821644fbaba67166fe0c498e126cd7d
Sarah Wooldridge <sawqxd@mail.missouri.edu>
2018-09-07 17:21:26 -0500

    First Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Sarah Wooldridge on 9/5/18.
+//  Copyright © 2018 Sarah Wooldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
